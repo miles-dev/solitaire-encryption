f393793befced5ec0bb3f7b992f84ad20573b967
miles-dev <miles-dev>
2016-07-13 19:59:09 -0400

    Optimized user interaction code to re-use CLI logic.

--- src/core/Execute.java
+++ src/core/Execute.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,47 +11,36 @@ public class Execute
 	{
 		// Get input from the user:
 		Scanner reader = new Scanner(System.in);
-		System.out.print("Enter phrase: ");
-		String input = reader.nextLine();
+		List<String> options = new ArrayList<String>();
+		String phrase = getUserInput(reader, "Enter phrase: ", options).toLowerCase();
 		
-		String methodRequest = "Enter e for encryption, enter d for decryption:";
-		System.out.print(methodRequest);
-		String method = reader.next().toLowerCase();
-		while (!method.equals("e") && !method.equals("d"))
-		{
-			System.out.print(methodRequest);
-			method = reader.next();
-		}
+		options.clear();
+		options.addAll(Arrays.asList("E","D"));
+		String method = getUserInput(reader, "Enter e for encryption, enter d for decryption: ", options);
 		
-		String deckOrderRequest = "Enter o for an ordered deck, enter r for a random deck, enter s to specify your own deck:";
-		System.out.print(deckOrderRequest);
-		String deckOrder = reader.next().toLowerCase();
-		while (!deckOrder.equals("o") && !deckOrder.equals("r") && !deckOrder.equals("s"))
-		{
-			System.out.print(deckOrderRequest);
-			deckOrder = reader.next();
-		}
+		options.clear();
+		options.addAll(Arrays.asList("O","R","S"));
+		String deckOrder = getUserInput(reader, "Enter o for an ordered deck, enter r for a random deck, enter s to specify your own deck: ", options);
 		
 		String specifiedDeck = "";
-		if (deckOrder.equals("s"))
-			while (specifiedDeck.isEmpty())
-			{
-				System.out.print("Enter order for your deck (Ex: [C1,C2,...,JA53,...D17,...JB53,S48]:");
-				specifiedDeck = reader.nextLine();
-			}
-		
-		System.out.println("phrase:" + input);
+		if (deckOrder.equals("S"))
+		{
+			options.clear();
+			specifiedDeck = getUserInput(reader, "Enter order for your deck (Ex: [C1,C2,...,JA53,...D17,...JB53,S48]: ", options);
+		}
+			
+		System.out.println("phrase:" + phrase);
 		System.out.println("Encrypt/Decrypt:" + method);
 		System.out.println("Deck Order:" + deckOrder);
 		reader.close();
 		
-		Solitaire runner = new Solitaire(input);
+		Solitaire runner = new Solitaire(phrase);
 
 		// Decide if you want to use an ordered, random, or a specified deck.
 		List<Card> generatedDeck = null;
-		if (deckOrder.equals("o"))
+		if (deckOrder.equals("O"))
 			runner.createOrderedDeck();
-		else if (deckOrder.equals("r"))
+		else if (deckOrder.equals("R"))
 		{
 			runner.createRandomDeck();
 			generatedDeck = runner.getDeck().getCards();
@@ -59,7 +50,7 @@ public class Execute
 		
 		runner.createKey();
 		String output;
-		if (method.equals("e"))
+		if (method.equals("E"))
 			output = (runner.encrypt());
 		else
 			output = runner.decrypt(runner.getOfficial());
@@ -75,5 +66,27 @@ public class Execute
 		System.out.println("Output:  " + officialOutput);
 		if (deckOrder.equals("r"))
 			System.out.println("Generated Deck Order was:\n" + generatedDeck);
+	}
+	
+	/**
+	 * Helper to pull out all of the logic for getting input from the user.
+	 * @param reader - Scanner to pull input from the user.
+	 * @param prompt - Prompt telling the user what input is needed.
+	 * @param options - Where applicable, options the user is allowed to provide. 
+	 * 			It will loop until one of the options is entered by the user.
+	 * @return Input from the user that meets the options requirement if applicable.
+	 */
+	private static String getUserInput(Scanner reader, String prompt, List<String> options)
+	{
+		System.out.print(prompt);
+		String input = reader.nextLine().toUpperCase();
+		
+		// If there are no options to check against, accept whatever the user enters.
+		while (options.size() > 0 && !options.contains(input))
+		{
+			System.out.print(prompt);
+			input = reader.nextLine().toUpperCase();
+		}
+		return input;
 	}
 }
