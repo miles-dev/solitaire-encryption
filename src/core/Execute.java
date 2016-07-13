b59236725da0d110fb8c09e4f8de48cd07b434c2
miles-dev <miles-dev>
2016-07-12 21:23:09 -0400

    Pull temporary documentation out of Execute.java. Update README.md to include a more complete documentation.

--- src/core/Execute.java
+++ src/core/Execute.java
@@ -1,29 +1,4 @@
 package core;
-/* Solitaire Ecryption Program
-   miles-dev
-
- Deck will be in order i.e. A-K, CDaHbS; a = Joker A, b = Joker B
- Each card has a value which is used when encrypting/decrypting an input:
- 	* C (Clubs): 1-13
- 	* D (Diamonds): 14-26
- 	* H (Hearts): 27-39
- 	* S (Spades): 39-52
- 	* Jokers: 53
- Joker A has a num of -1, and Joker B has a num of -2
- Steps for the encryption:
-	1. Move Joker A down 1 card.
-	2. Move Joker B down 2 cards.
-	3. Triple cut: switch the cards before the 1st joker with
-		those after the 2nd joker.
-	4. Count Cut: look at the bottom card, count down from the
-		top of the deck that many cards. Cut after the
-		card you counted too. Keep the bottom card the same.
-	5. Look at the top card, and count down that many cards from
-		the top. If you hit a joker, start with step 1 again.
-		Otherwise, that is the first value for the key.
-	6. Store that value and restart from step 1 for as many times
-		as needed.
-*/
 
 public class Execute 
 {
